f302467808eb7e2bd982cc9d8dca1902af421678
cmg6991 <110959882+cmg6991@users.noreply.github.com>
2024-01-03 19:13:30 +0900

    Create ShoebillEngine.cpp

---
+++ ShoebillEngine/ShoebillEngine.cpp
@@ -0,0 +1,26 @@
+#include "ShoebillEngine.h"
+
+ShoebillEngine::ShoebillEngine()
+{
+
+}
+
+ShoebillEngine::~ShoebillEngine()
+{
+
+}
+
+void ShoebillEngine::Initalize()
+{
+
+}
+
+void ShoebillEngine::Update()
+{
+
+}
+
+void ShoebillEngine::Render()
+{
+
+}
