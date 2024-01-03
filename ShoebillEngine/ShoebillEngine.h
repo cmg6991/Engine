da68cbcc03c2a320c07950b6b7fe5a0278104b56
cmg6991 <110959882+cmg6991@users.noreply.github.com>
2024-01-03 19:13:54 +0900

    Create ShoebillEngine.h

---
+++ ShoebillEngine/ShoebillEngine.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/// <summary>
+/// 엔진 시작한당
+/// 2024.01.03
+/// </summary>
+class ShoebillEngine
+{
+public:
+	ShoebillEngine();
+	~ShoebillEngine();
+
+	void Initalize();
+	void Update();
+	void Render();
+};
+
